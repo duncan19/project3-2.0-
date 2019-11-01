619b9d3059f433ceb7ce5b53fbbf8278923df397
Duncan Robbins <duncanrobbins19@gmail.com>
2019-11-01 11:26:59 -0700

    alter the results area of the webpage

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
   $("#userNumForm").submit(function(event){
     event.preventDefault();
     $("#userNumForm").hide();
+    $("#result").show();
     var arrayOfUserNUm=[];
     var usersNumber = parseInt($("input#userinputtedNumber").val());
       console.log(usersNumber)
