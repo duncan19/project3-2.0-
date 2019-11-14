2e74434c6a9d9f016fa95bb015c9aa01f7c95a31
Duncan Robbins <duncanrobbins19@gmail.com>
2019-11-13 21:20:41 -0800

    alter the code so that it is pushing the correct result and seperate ui

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,40 @@
+var arrayOfUserNUm=[];
+
+
 $(document).ready(function(){
   $("#userNumForm").submit(function(event){
     event.preventDefault();
     $("#userNumForm").hide();
-    var arrayOfUserNUm=[];
     var usersNumber = parseInt($("input#userinputtedNumber").val());
-      console.log(usersNumber)
-      var numtoword = function(){
-      console.log("test")
+
+      var moveToArrayFunc  = function(){
+
       var usersnumchange = usersNumber
       for(i=1; i <= usersNumber; i++){
         arrayOfUserNUm.push(usersnumchange)
-
         usersnumchange -= 1;
         }
         for (i = 0; i < arrayOfUserNUm.length; i ++){
          if(arrayOfUserNUm[i].toString().includes(3))
          {
-           arrayOfUserNUm[i] = 'im sorry dave i cant od that';
+                arrayOfUserNUm.push('Im sorry I can not do that')
          }
          else if (arrayOfUserNUm[i].toString().includes(2))
          {
-           arrayOfUserNUm[i] = 'Boop';
+                arrayOfUserNUm.push('boop')
          }
          else if (arrayOfUserNUm[i].toString().includes(1))
          {
-           arrayOfUserNUm[i] = 'Beep';
+           arrayOfUserNUm.push('beep')
+         }
+         else{
+           arrayOfUserNUm.push(arrayOfUserNUm[i])
          }
        }
-       console.log((arrayOfUserNUm))
        var arrayforresult = arrayOfUserNUm.toString();
        $("#result").text(arrayforresult)
        $("#result").show();
 };
-console.log(numtoword())
+
 });
 });
